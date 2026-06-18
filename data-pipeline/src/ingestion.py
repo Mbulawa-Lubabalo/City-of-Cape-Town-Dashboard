487526dc677531b5f5e20c8377c17f53b1eb3d15
Lubabalo Mbulawa <lmbulawa4@gmail.com>
2026-06-18 17:08:28 +0200

    fetched the service request data

---
+++ data-pipeline/src/ingestion.py
@@ -0,0 +1,18 @@
+import requests
+
+
+
+def Fetch_Data():
+
+    url = "https://services6.arcgis.com/nyYfO9SxHU2ChQd9/arcgis/rest/services/Service_Requests_2023_until_20_May_2026/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Accept": "application/json"
+    }
+    
+    response = requests.get(url, headers=HEADERS, timeout=10)
+    return response.json()
+
+if __name__ == "__main__":
+    Fetch_Data()
+
