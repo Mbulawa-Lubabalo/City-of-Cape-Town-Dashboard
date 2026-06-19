c6a8858fb342ceb4b4b414418ca2fd1e384defa6
Lubabalo Mbulawa <lmbulawa4@gmail.com>
2026-06-20 00:31:39 +0200

    renamed C3_Complaint_Type to Complaint_Type

--- data_pipeline/transform.py
+++ data_pipeline/transform.py
@@ -6,7 +6,7 @@ import json
 url = "https://services6.arcgis.com/nyYfO9SxHU2ChQd9/arcgis/rest/services/Service_Requests_2023_until_20_May_2026/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
 
 
-def DF_Data(rawdata: dict):
+def DF_Data(rawdata: dict) -> pd.DataFrame:
     features = rawdata["features"]
     
     df = pd.DataFrame([f["properties"] for f in features])
@@ -14,7 +14,15 @@ def DF_Data(rawdata: dict):
     print(df.head())
     return df
 
+def Clean_Data(data: pd.DataFrame):
+    # Rename columns
+    data = data.rename(columns={'C3_Complaint_Type': 'Complaint_Type'})
+
+    
+    print(data)
+    return data
 
 if __name__ == "__main__":
     rawdata = Fetch_Data(url)
-    DF_Data(rawdata)
+    dataFrame_Data = DF_Data(rawdata)
+    Clean_Data(dataFrame_Data)
